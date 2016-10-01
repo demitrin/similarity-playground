d38b31bce2174a6c338de1614bb60e2a72d001ba
Demitri Nava <demitri@mit.edu>
2016-10-01 00:23:25 -0400

    add more randomness to generated datasets

--- generate_str_csv.py
+++ generate_str_csv.py
@@ -11,7 +11,7 @@ def generate_str_csv(length=100000, filename='random_strs_1.csv'):
         for i in range (length):
             if i % 10000 == 0:
                 print 'wrote element {} to file {}'.format(i + 1, filename)
-            f.write(''.join([choice(string.digits) for _ in range(5)]) + '\n')
+            f.write(''.join([choice(string.digits) for _ in range(randint(2, 8))]) + '\n')
         print 'finished writing {} elements to file '.format(length, filename)
 
 if __name__ == '__main__':
