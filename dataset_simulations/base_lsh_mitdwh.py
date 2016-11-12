9aad048756d52540d06978ea12a7912680def73f
Demitri Nava <demitri@mit.edu>
2016-11-11 20:45:42 -0500

    Add multithreaded capabilities

--- dataset_simulations/base_lsh_mitdwh.py
+++ dataset_simulations/base_lsh_mitdwh.py
@@ -1,28 +1,36 @@
 import csv
 import glob
+from multiprocessing import Pool
 
 import os
 
 from datasketch import MinHashLSH
 
 
-def base_lsh_mitdwh(mitdwh_path, minhash_cls):
-    minhashes = []
-    for filename in glob.glob(os.path.join(mitdwh_path, '*.csv')):
-        with open(filename, 'r') as f:
-            reader = csv.reader(f)
-            csv_minhashes = []
-            for row in reader:
-                if not csv_minhashes:
-                    for i in range(len(row)):
-                        csv_minhashes.append(minhash_cls())
-
+def get_csv_minhashes(filename, minhash_cls):
+    with open(filename, 'r') as f:
+        print 'working on file ' + filename
+        reader = csv.reader(f)
+        csv_minhashes = []
+        for row in reader:
+            if not csv_minhashes:
                 for i in range(len(row)):
-                    csv_minhashes[i].update(row[i])
+                    csv_minhashes.append(minhash_cls())
 
-            for minhash in csv_minhashes:
-                minhashes.append(minhash)
+            for i in range(len(row)):
+                csv_minhashes[i].update(row[i])
+    return csv_minhashes
 
+
+def async_get_csv_minhashes(filename__minhash_cls):
+    return get_csv_minhashes(*filename__minhash_cls)
+
+
+def base_lsh_mitdwh(mitdwh_path, minhash_cls):
+    pool = Pool(4)
+    args = map(lambda filename: (filename, minhash_cls), glob.glob(os.path.join(mitdwh_path, '*.csv')))
+    csv_minhashes = pool.map(async_get_csv_minhashes, args)
+    minhashes = reduce(lambda csv_minhash1, csv_minhash2: csv_minhash1 + csv_minhash2, csv_minhashes, [])
     lsh = MinHashLSH(threshold=.3)
     for i in range(len(minhashes)):
         lsh.insert(i, minhashes[i])
