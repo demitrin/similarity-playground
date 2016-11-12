8b7c0dfd4748a364a1a07b4b0b624d204e34b073
Demitri Nava <demitri@mit.edu>
2016-11-11 22:42:00 -0500

    Remove unnecessary code from exact similarity check

--- random_num_simulations/calc_exact_similarity.py
+++ random_num_simulations/calc_exact_similarity.py
@@ -4,37 +4,35 @@ Usage:
   calc_exact_similarity.py <file1> <file2>
 """
 import csv
+
 from docopt import docopt
+
+
 def calc_real_similarity(file1, file2):
     hashed_value_counts = {}
-    len_file1 = 0
     with open(file1, 'r') as f:
         reader = csv.reader(f)
         data1 = list(reader)
         data1 = map(lambda arr: arr[0], data1)
-        len_file1 = len(data1)
         for val in data1:
             hashed_value_counts[val] = hashed_value_counts.get(val, 0) + 1
-    
+
     len_intersection = 0
-    len_file2 = 0
     with open(file2, 'r') as f:
         reader = csv.reader(f)
         data2 = list(reader)
         data2 = map(lambda arr: arr[0], data2)
-        len_file2 = len(data2)
         for val in data2:
             if val in hashed_value_counts:
                 hashed_value_counts[val] = hashed_value_counts[val] - 1
                 if hashed_value_counts[val] == 0:
-                    del(hashed_value_counts[val])
+                    del (hashed_value_counts[val])
 
                 len_intersection += 1
     s1 = set(data1)
     s2 = set(data2)
-    actual_jaccard = float(len(s1.intersection(s2)))/float(len(s1.union(s2)))
+    actual_jaccard = float(len(s1.intersection(s2))) / float(len(s1.union(s2)))
     return actual_jaccard
-    #return float(len_intersection) / (float(len_file1 + len_file2 - len_intersection))
 
 
 if __name__ == '__main__':
