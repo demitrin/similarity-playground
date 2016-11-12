7c670a8c2e5d30cc6b9b97ce0d7808ebcce814ba
Demitri Nava <demitri@mit.edu>
2016-11-12 00:26:12 -0500

    formatting

--- random_num_simulations/run_simulations.py
+++ random_num_simulations/run_simulations.py
@@ -4,11 +4,13 @@ Usage:
   run_simulations.py <num_sets> <size_of_sets> <num_buckets>
 """
 import datetime
-import os
 from multiprocessing import Pool
+
+import os
 from docopt import docopt
-from generate_str_csv import generate_str_csv
+
 from calculate_similarities import calculate_similarities
+from generate_str_csv import generate_str_csv
 
 
 def get_file_names(num_sets):
@@ -33,6 +35,7 @@ def async_generate_str_csv(size_of_sets__file_name):
 def get_similarity_error(similarities, approx_similarity_key):
     return map(lambda similarity: abs(1 - (similarity[approx_similarity_key]
                                            / similarity['exact_similarity'])), similarities)
+
 
 def run_similarity_simulations(num_sets, size_of_sets, num_buckets):
     LOCAL_CSV_TESTS = 'local_csv_tests'
@@ -62,9 +65,12 @@ def run_similarity_simulations(num_sets, size_of_sets, num_buckets):
     approx_similarities_errors = get_similarity_error(similarities, 'approx_similarity')
     bucketed_approx_similarities_errors = get_similarity_error(similarities, 'bucketed_approx_similarity')
     ophr_similarities_errors = get_similarity_error(similarities, 'ophr_similarity')
-    approx_similarity_str = 'Average approx similarity error {}'.format(sum(approx_similarities_errors) / len(approx_similarities_errors))
-    bucketed_approx_similarity_str =  'Average bucketed approx similarity error {}'.format(sum(bucketed_approx_similarities_errors) / len(bucketed_approx_similarities_errors))
-    ophr_similarities_errors = 'Average OPHR similarity error {}'.format(sum(ophr_similarities_errors) / len(ophr_similarities_errors))
+    approx_similarity_str = 'Average approx similarity error {}'.format(
+        sum(approx_similarities_errors) / len(approx_similarities_errors))
+    bucketed_approx_similarity_str = 'Average bucketed approx similarity error {}'.format(
+        sum(bucketed_approx_similarities_errors) / len(bucketed_approx_similarities_errors))
+    ophr_similarities_errors = 'Average OPHR similarity error {}'.format(
+        sum(ophr_similarities_errors) / len(ophr_similarities_errors))
 
     output_filename = os.path.join(working_dir, 'output.txt')
     with open(output_filename, 'w+') as f:
@@ -72,6 +78,8 @@ def run_similarity_simulations(num_sets, size_of_sets, num_buckets):
         f.write(bucketed_approx_similarity_str + '\n')
         f.write(ophr_similarities_errors + '\n')
         print 'wrote outputs to {}'.format(output_filename)
+
+
 if __name__ == '__main__':
     arguments = docopt(__doc__)
     num_sets = int(arguments['<num_sets>'])
